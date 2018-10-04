47ce8d14b838913802879c9c1f4f436707f556c5
Chirag goyal <31381075+chiragoyal@users.noreply.github.com>
2018-10-04 12:16:45 +0530

    Add files via upload

---
+++ Exception.java
@@ -0,0 +1,17 @@
+class Test 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello World!");
+		try
+		{
+		    System.out.println(10/0);
+	}
+catch(ArithmeticException e)
+	{
+
+	System.out.println(10/2);
+	}
+	System.out.println("main");
+}
+}
---
+++ throw.java
@@ -0,0 +1,10 @@
+class Test extends RuntimeException
+{
+static ArithmeticException e;
+	public static void main(String[] args) 
+	{
+		throw new Test();
+		
+	
+	}
+}
